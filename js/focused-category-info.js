dfab5a8a27402d6ac5da22b74c957bd9898cb287
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2021-03-03 09:47:03 -0500

    Update focused-category-info.js

--- js/focused-category-info.js
+++ js/focused-category-info.js
@@ -164,13 +164,13 @@ angular.module('app', []).controller('gitHubDataController', [
 
                 //create function for generating hash url for each repo
                 $scope.repoHref = function(nametag) {
-                    $window.location.href = cpath('repo#' + nametag);
+                    $window.location.href = cpath('repo/#/' + nametag);
                 };
 
                 //function to generate hash url for each category
                 $scope.categoryHref = function(nametag) {
                     var result = nametag.replace(/ /g, '');
-                    $window.location.href = cpath('category#' + result);
+                    $window.location.href = cpath('category/#/' + result);
                 };
             });
         });
