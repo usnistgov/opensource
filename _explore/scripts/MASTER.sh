abb38edfe0ccf1610181e3e31e8ae4c20e445023
usnistgov <usnistgov@users.noreply.github.com>
2025-03-26 19:16:35 -0400

    Security Fixes.

--- _explore/scripts/MASTER.sh
+++ _explore/scripts/MASTER.sh
@@ -39,37 +39,54 @@ echo -e "START\t$(date -u)" >> $DATELOG
 
 
 # RUN THIS FIRST
-runScript cleanup_inputs.py
-
+runScript cleanup_inputs.py &
+cleanup_inputs=$!
 
 # --- BASIC DATA ---
 # Required before any other repo scripts (output used as repo list)
-runScript get_repos_info.py
-# Required before any other member scripts (output used as member list)
-runScript get_nist_members.py
+runScript get_repos_info.py &
+get_repos_info=$!
 
+# Required before any other member scripts (output used as member list)
+runScript get_nist_members.py &
+get_nist_members=$!
 
 # --- EXTERNAL V INTERNAL ---
-runScript get_members_extrepos.py
-runScript get_repos_users.py
+runScript get_members_extrepos.py &
+get_members_extrepos=$!
 
+runScript get_repos_users.py &
+get_repos_users=$!
 
 # --- ADDITIONAL REPO DETAILS ---
-runScript get_repos_languages.py
-runScript get_repos_topics.py
-runScript get_repos_activitycommits.py
-runScript get_repos_activitylines.py
-runScript get_repos_dependencies.py
+runScript get_repos_languages.py &
+get_repos_languages=$!
+
+runScript get_repos_topics.py &
+get_repos_topics=$!
+
+runScript get_repos_activitycommits.py &
+get_repos_activitycommits=$!
+
+runScript get_repos_activitylines.py &
+get_repos_activitylines=$!
+
+runScript get_repos_dependencies.py &
+get_repos_dependencies=$!
 
 
 # --- HISTORY FOR ALL TIME ---
-runScript get_repos_creationhistory.py
+runScript get_repos_creationhistory.py &
+get_repos_creationhistory=$!
 
 
 # RUN THIS LAST
-runScript build_yearlist.py  # Used in case of long term cumulative data
+runScript build_yearlist.py &  # Used in case of long term cumulative data
+build_yearlist=$!
 
-runScript gather_repo_metadata.py  # Generate simplified metadata file
+runScript gather_repo_metadata.py &  # Generate simplified metadata file
+gather_repo_metadata=$!
 
+wait $cleanup_inputs $get_repos_info $get_nist_members $get_members_extrepos $get_repos_users $get_repos_languages $get_repos_topics $get_repos_activitycommits $get_repos_activitylines $get_repos_dependencies $get_repos_creationhistory $build_yearlist $gather_repo_metadata
 
 echo "MASTER UPDATE COMPLETE"
